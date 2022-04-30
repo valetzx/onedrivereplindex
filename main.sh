5e050bb3151bfb520a8fd855250beba6601bbf8a
valetzx <45054400+valetzx@users.noreply.github.com>
2022-04-30 12:17:49 +0800

    Update main.sh

--- main.sh
+++ main.sh
@@ -1,9 +1,10 @@
-#rm -rf onedrive 
-#git clone https://github.com/spencerwooo/onedrive-vercel-index onedrive
+rm -rf onedrive 
+git clone https://github.com/spencerwooo/onedrive-vercel-index onedrive
+
+#第一次运行完成后请注释掉以上指令！
+
 cd onedrive 
 pnpm install 
 pnpm build
-
 pnpm start &
-
 redis-server ../redis.conf --port 6666
